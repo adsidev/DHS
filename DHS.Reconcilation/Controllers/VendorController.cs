7c31d7e269c639f607d299318f360fd495b0abd4
Venu Madhav <adi_dev@goadsi.com>
2022-02-04 15:40:02 +0530

    Added the basic info for Vendor

---
+++ DHS.Reconcilation/Controllers/VendorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Medicaid.Reconcilation.Controllers
+{
+    public class VendorController : Controller
+    {
+        // GET: Vendor
+        public ActionResult ManageVendors()
+        {
+            return View();
+        }
+
+        public ActionResult CreateVendor()
+        {
+            return View();
+        }
+    }
+}
