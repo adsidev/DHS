e495963ece8d9f5bbf83ed31dd18ccfc54a5ef1a
Venu Madhav <mahitechnosystems@gmail.com>
2022-04-24 21:58:16 +0530

    Manage Receivable Report

--- DHS.Reconcilation/Controllers/ReportController.cs
+++ DHS.Reconcilation/Controllers/ReportController.cs
@@ -383,5 +383,96 @@ namespace DHS.Reconcilation.Controllers
                 return RedirectToAction("Error", "Home");
             }
         }
+
+        [HttpGet]
+        public async Task<ActionResult> ManageReceivablesReport()
+        {
+            if (!Common.SessionExists())
+                return RedirectToAction("Index", "Home");
+            Session["FGReport"] = "0";
+            ReportResponse reportResponse = new ReportResponse();
+            string url = strBaseURL + "Report/GetGrantProjectReport";
+            // string url = strBaseURL + "AuditReport/GetCRSummaryReport";
+            client.BaseAddress = new Uri(url);
+            ReportRequest reportRequest = new ReportRequest();
+            reportRequest.FiscalYearId = 0;
+            reportRequest.ProjectId = 0;
+            HttpResponseMessage responseMessage = await client.PostAsJsonAsync(url, reportRequest);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+                reportResponse = JsonConvert.DeserializeObject<ReportResponse>(responseData);
+                if (reportResponse.Message == string.Empty && reportResponse.ErrorMessage == string.Empty)
+                {
+                    string PageName = "Reports";
+                    reportResponse.rolePermissionEntity = Common.PagePermissions(PageName); bool PageHasPermissionsOrNot = CheckPagePermissionHeadders.PageHasPermission(PageName);
+                    if (!PageHasPermissionsOrNot)
+                        return RedirectToAction("Index", new { ErrorMsg = "You do not have access to this activity. Please contact your administrator." });
+                    ViewData["Project"] = "0";
+                    ViewData["FiscalYear"] = "0";
+                    return View(reportResponse);
+                }
+                else
+                {
+                    TempData["LoginFailure"] = reportResponse.Message;
+                    return RedirectToAction("Error", "Home");
+                }
+            }
+            else
+            {
+                TempData["LoginFailure"] = responseMessage.ToString();
+                return RedirectToAction("Error", "Home");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> ManageReceivablesReport(ReportResponse reportResponse)
+        {
+            if (!Common.SessionExists())
+                return RedirectToAction("Index", "Home");
+
+            if (Request.Form["ProjectId"] == "")
+                ViewData["Project"] = "0";
+            else
+                ViewData["Project"] = Convert.ToInt32(Request.Form["ProjectId"]);
+            if (Request.Form["FiscalYearId"] == "")
+                ViewData["FiscalYear"] = "0";
+            else
+                ViewData["FiscalYear"] = Convert.ToInt32(Request.Form["FiscalYearId"]);
+            Session["FGReport"] = "0";
+            ReportRequest reportRequest = new ReportRequest();
+            reportRequest.ProjectId = Convert.ToInt32(ViewData["Project"]);
+            reportRequest.FiscalYearId = Convert.ToInt32(ViewData["FiscalYear"]);
+
+            string url = strBaseURL + "Report/GetGrantProjectReport";
+            client.BaseAddress = new Uri(url);
+            HttpResponseMessage responseMessage = await client.PostAsJsonAsync(url, reportRequest);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+                reportResponse = JsonConvert.DeserializeObject<ReportResponse>(responseData);
+                if (reportResponse.Message == string.Empty && reportResponse.ErrorMessage == string.Empty)
+                {
+                    string PageName = "Reports";
+                    reportResponse.rolePermissionEntity = Common.PagePermissions(PageName); bool PageHasPermissionsOrNot = CheckPagePermissionHeadders.PageHasPermission(PageName);
+                    if (!PageHasPermissionsOrNot)
+                        return RedirectToAction("Index", new { ErrorMsg = "You do not have access to this activity. Please contact your administrator." });
+
+                    return View(reportResponse);
+                }
+                else
+                {
+                    TempData["LoginFailure"] = reportResponse.Message;
+                    return RedirectToAction("Error", "Home");
+                }
+            }
+            else
+            {
+                TempData["LoginFailure"] = responseMessage.ToString();
+                return RedirectToAction("Error", "Home");
+            }
+        }
     }
 }
